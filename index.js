3257cd36963d454edc3396ff81a0c8d05c91b32a
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-01 21:39:41 -0400

    small updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('Hi claire, your boyfriend is a genius');
 });
 
 app.listen(3000, () => {
