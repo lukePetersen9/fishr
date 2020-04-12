15c7cf1efb126a921e005a0bf65022368e3cab91
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-12 17:20:28 -0400

    added api

--- index.js
+++ index.js
@@ -27,13 +27,15 @@ app.listen(3000, () => {
 });
 
 app.post('/makeuser', (req, res) => {
-
-    console.log('got something...');
-
-    connection.query('INSERT INTO users (first_name, last_name, username, email, userkey,phone_number,profile_picture) VALUES ("' + req.body.first + '","' + req.body.last + '","' + req.body.username + '","' + req.body.email + '","' + req.body.userID + '","' + req.body.phone + '","' + req.body.profilePicure + '")', function (err, rows, fields) {
-        if (err) throw err;
-
-        console.log(req.body.userID);
+    console.log('Making user: ' + req.body.userID);
+    connection.query('INSERT INTO users (first_name, last_name, username, email, userkey,phone_number,profile_picture, created) VALUES ("' + req.body.first + '","' + req.body.last + '","' + req.body.username + '","' + req.body.email + '","' + req.body.userID + '","' + req.body.phone + '","' + req.body.profilePicure + '","' + Date.now().toString + '")', function (err, rows, fields) {
+        if (err) {
+            console.log('Unable to make user: ' + req.body.userID + ', ' + err.code);
+            res.send(err.code);
+        } else {
+            console.log('Made user: ' + req.body.userID);
+            res.send(0);
+        }
     });
-    res.send('all good');
+
 });
