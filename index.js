2a26acd099f115f4dcd6228a6b10a0b656479e8f
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-12 19:22:51 -0400

    h

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ app.listen(3000, () => {
 
 app.post('/makeuser', (req, res) => {
     console.log('Making user: ' + req.body.userID);
-    connection.query('INSERT INTO users (first_name, last_name, username, email, userkey,phone_number,profile_picture, created) VALUES ("' + req.body.first + '","' + req.body.last + '","' + req.body.username + '","' + req.body.email + '","' + req.body.userID + '","' + req.body.phone + '","' + req.body.profilePicure + '","' + Date.now().toString + '")', function (err, rows, fields) {
+    connection.query('INSERT INTO users (first_name, last_name, username, email, userkey,phone_number,profile_picture, created) VALUES ("' + req.body.first + '","' + req.body.last + '","' + req.body.username + '","' + req.body.email + '","' + req.body.userID + '","' + req.body.phone + '","' + req.body.profilePicure + '","' + Date.now().toString() + '")', function (err, rows, fields) {
         if (err) {
             console.log('Unable to make user: ' + req.body.userID + ', ' + err.code);
             res.send(err.code);
         } else {
             console.log('Made user: ' + req.body.userID);
-            res.send(0);
+            res.send(err.code);
         }
     });
 
