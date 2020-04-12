b191a7f01934524676315e2a947971fde08b8e07
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-11 20:59:39 -0400

    commit?

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hi claire, your boyfriend is a genius');
+app.get('/makeuser', (req, res) => {
+    console.log(req.headers);
 });
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
+
+/*
+cognito: 4n3s8jd85je5prosfh7lsk39a8
+https://fishrauth.auth.us-east-2.amazoncognito.com
+ */
