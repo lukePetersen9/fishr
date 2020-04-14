3decdba0d29cbf2bec3b2109631ef4ebe2332abe
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-14 01:54:11 -0400

    d

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/makePost', (req, res) => {
     // s3.uploadFile(req.body.image2, req.body.userID);
     // s3.uploadFile(req.body.image3, req.body.userID);
     // s3.uploadFile(req.body.image4, req.body.userID);
-    s3.uploadFile(req.body.video1, req.body.userID);
+    //s3.uploadFile(req.body.video1, req.body.userID);
     // s3.uploadFile(req.body.video2, req.body.userID);
     // s3.uploadFile(req.body.video3, req.body.userID);
     // s3.uploadFile(req.body.video4, req.body.userID);
