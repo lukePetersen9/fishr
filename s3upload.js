17d995fd450116dad1d485ec1dff2ea33930a780
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-16 15:31:21 -0400

    access?

--- s3upload.js
+++ s3upload.js
@@ -26,6 +26,7 @@ var upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: 'posts',
+        acl: 'public-read',
         metadata: function (req, file, cb) {
             cb(null, {
                 fieldName: file.fieldname
