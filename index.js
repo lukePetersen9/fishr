0f87764a6a99e68687d3bed70d89a70b478e907a
lukePetersen9 <lukepetersen29@gmail.com>
2020-04-16 16:10:40 -0400

    sfd

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.post('/makeuser', (req, res) => {
 });
 
 app.post('/makePost', s3.upload.array('picture', 8), function (req, res, next) {
+    console.log(req.files[0].fileName);
     res.send('Successfully uploaded ' + ' files!');
 });
 
